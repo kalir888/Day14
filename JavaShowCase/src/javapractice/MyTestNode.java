cac5e7c5d10705cc1d62b66e84d0c78fcdaaa762
Kalir888 <kalir888@gmail.com>
2022-03-31 11:10:05 +0530

    Completed the uc1

---
+++ JavaShowCase/src/javapractice/MyTestNode.java
@@ -0,0 +1,21 @@
+package javapractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyTestNode {
+    @Test
+    public static void main(String args[]) {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                mySecondNode.getNext().equals(myThirdNode);
+        System.out.println(result);
+    }
+
+}
